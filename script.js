e49ec5765160bd8aef5bfa1b2fe4481e267e1fa2
MixCodes <patipan1528@gmail.com>
2022-05-27 18:48:14 +0700

    Updating

--- script.js
+++ script.js
@@ -1,15 +1,36 @@
+let seconds = 10;
+let defaultMin = 500;
+let defaultMax = 1000;
+let defaultSpeed = 10;
+let defaultlivesLeft = 5;
+const gameOverEnabled = true;
+let difficulty = 0;
+
 const holes = document.querySelectorAll(".hole");
 const moles = document.querySelectorAll(".mole");
-const scoreBoard  = document.getElementById("score");
-const timer = document.getElementById("timer");
 let lastHole;
+const scoreSpan  = document.getElementById("score-board");
+const highscoreSpan = document.getElementById("highscore");
+const timerSpan = document.getElementById("timer");
+const lives = document.getElementById("lives");
+const startButton = document.getElementById("start");
+const defaultStartButtonText = startButton.textContent;
+
 let timeUp = false;
 let started = false;
 let score = 0;
-let milliseconds = 10000;
-let timeLeft = milliseconds / 10;
-let min = 500;
-let max = 1500;
+let livesLeft = 0;
+let timeLeft = seconds;
+let highscore = highscoreSpan.textContent;
+// let missed = 0
+const gameTimeOut = setTimeout(function() {
+    started = false;
+    retry();
+	}, seconds * 1000);
+
+let moleTimeOut;
+
+let timer;
 
 
 function getRandomInt(min, max) {
@@ -27,24 +48,61 @@ function getRandomHole(holes){
 }
 
 function pop() {
-   	const time = getRandomInt(500, 1000);
+	let speed = defaultSpeed;
+	// if (score > speed) {
+	speed = score;
+	if (defaultMin - speed * 10 <= 10) {
+	speed = defaultMin - 10
+	}
+	let min = defaultMin - ((speed * 10) * 1 + (difficulty / 10));
+	let max = defaultMax - ((speed * 10) * 1 + (difficulty / 10));
+   	const time = getRandomInt(min, max);
     const hole = getRandomHole(holes);
     hole.classList.add("up");
-    setTimeout(() => {
-      	hole.classList.remove("up");
+    moleTimeOut = window.setTimeout(function() {
+     	if (hole.classList.contains("up")) {
+    		hole.classList.remove("up");
+    		livesLeft --;
+    		lives.textContent = livesLeft;
+    		gameOver();
+    	}
       	if (started) pop();
-    }, time);
+    }, time)
 }
  
 function start() {
 	if (started) return;
+	startButton.textContent = "เริ่มใหม่";
+	startButton.onclick = confirm;
 	started = true;
-	scoreBoard.textContent = 0;
+	scoreSpan.textContent = 0;
+	timerSpan.textContent = timeLeft;
 	score = 0;
+	missed = 0;
+	livesLeft = defaultlivesLeft;
+	lives.textContent = defaultlivesLeft;
 	pop();
-	setTimeout(() => {
+	countdown();
+	gameTimeOut;
+
+	// setTimeout(() => {
+	// 	started = false;
+	// }, milliseconds);
+}
+
+function gameOver() {
+	if (!gameOverEnabled) return;
+	if (livesLeft <= 0) {
 		started = false;
-	}, milliseconds);
+		clearTimeout(gameTimeOut);
+		clearTimeout(moleTimeOut);
+		clearTimeout(timer);
+		timerSpan.textContent = 0;
+		checkHighscore();
+		// timerSpan.textContent = 0
+		lastHole.classList.remove("up");
+		retry();
+	}
 }
 
 function whack(click) {
@@ -53,7 +111,48 @@ function whack(click) {
 		score ++;
 		this.parentNode.classList.remove("up");
 	}
-	scoreBoard.textContent = score;
+	scoreSpan.textContent = score;
+}
+
+function retry() {
+	startButton.textContent = "เริ่มใหม่";
+	startButton.onclick = start;
+}
+
+function confirm() {
+	startButton.style.color = "#FFFFFF";
+	startButton.style.backgroundColor = "#E6564E";
+	startButton.textContent = "กดอีกรอบเพื่อเริ่มใหม่";
+	startButton.onclick = confirmed;
+	
+}
+
+function confirmed() {
+	livesLeft = 0
+	gameOver()
+	start();
+	startButton.style.color = "#E6564E";
+	startButton.style.backgroundColor = "#FFFFFF";
+	startButton.textContent = "เริ่มใหม่";
+}
+
+function checkHighscore() {
+	if (score > highscore && !started) {
+		highscore = score;
+		highscoreSpan.textContent = score;
+		localStorage.setItem("highscore", score);
+	}
+}
+
+function countdown() {	
+	timer = window.setTimeout(function() {
+		if (timerSpan.textContent <= 0) {
+			checkHighscore();
+			return;
+		}
+		timerSpan.textContent--;
+		countdown();
+	}, 1000);
 }
 
 moles.forEach(mole => mole.addEventListener('click', whack));
